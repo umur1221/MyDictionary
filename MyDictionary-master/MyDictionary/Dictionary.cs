a2c96eaa0941ebd269aafdf8930731b11eaf9c80
umur1221 <68390241+umur1221@users.noreply.github.com>
2021-01-23 19:54:58 +0300

    Add files via upload

---
+++ MyDictionary-master/MyDictionary/Dictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Dictionary<B, O>
+    {
+        B[] id;
+        B[] tempId;
+        O[] name;
+        O[] tempName;
+        public Dictionary()
+        {
+            id = new B[0];
+            name = new O[0];
+        }
+        public void Add(B key, O value)
+        {
+            tempId = id;
+            tempName = name;
+
+            id = new B[id.Length + 1];
+            name = new O[name.Length + 1];
+            for (int i = 0; i < tempName.Length; i++)
+            {
+                name[i] = tempName[i];
+                id[i] = tempId[i];
+            }
+            name[name.Length - 1] = value;
+            id[id.Length - 1] = key;
+
+            Console.WriteLine("ID: " + key + " İsim: " + value + " başarıyla eklendi.");
+        }
+    }
+}
---
+++ MyDictionary-master/MyDictionary/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dictionary = new Dictionary<int, string>();
+            dictionary.Add(1, "Buğrahan");
+            dictionary.Add(2, "Beyza");
+            dictionary.Add(3, "Batuhan");
+            dictionary.Add(4, "Selçuk");
+        }
+    }
+}
